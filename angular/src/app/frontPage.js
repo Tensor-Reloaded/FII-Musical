609c336b0380aaa5e930ca92323d361b4af3fdac
Bogdan405 <Bogdan405@outlook.com>
2021-10-31 13:32:43 +0200

    working on angular front end
    Alex & Bogdan

---
+++ angular/src/app/frontPage.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+  const fileSelector = document.getElementById('file-selector');
+  fileSelector.change(function(e) {
+    console.log("ASFDAFADASF");
+  });
+}
+
+var myExtObject = (function() {
+
+    return {
+      func1: function() {
+        alert('function 1 called');
+      },
+      func2: function() {
+        alert('function 2 called');
+      }
+    }
+
+})(myExtObject||{})
+
+var webGlObject = (function() {
+    return {
+      init: function() {
+        alert('webGlObject initialized');
+      }
+    }
+})(webGlObject||{})
